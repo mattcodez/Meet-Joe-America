515d6c4f0cbf27baec87fc9081892b0b426eebd5
Matthew Molnar <matthew.molnar@gmail.com>
2013-11-02 16:02:33 -0400

    Auto-resize in progress.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+"use strict";
 
+function init(){
+	var mainContainer = $('#mainContainer');
+	var windowObj = $(window);
+	
+	var ar = 825 / 650;
+	
+	windowObj.on('resize', autoResize);
+	autoResize();//Initial run
+
+	function autoResize(e){
+		mainContainer.css('width', windowObj.height() * ar);
+		mainContainer.css('height', windowObj.height());
+	}
+}
