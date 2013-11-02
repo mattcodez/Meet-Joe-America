33f59d8317afa7b6fd1017aa47a3fdf7bff50be9
Matthew Molnar <matthew.molnar@gmail.com>
2013-11-02 16:33:51 -0400

    Finishing up basic sizing changes.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,19 @@ function init(){
 	autoResize();//Initial run
 
 	function autoResize(e){
+		var windowWidth = windowObj.width();
+		var windowHeight = windowObj.height();
+		
 		mainContainer.css('width', windowObj.height() * ar);
 		mainContainer.css('height', windowObj.height());
+		
+		if (mainContainer.width() > windowWidth){
+			mainContainer.css('width', windowWidth);
+		}
+		
+		var margins = windowWidth - mainContainer.width();
+		if (margins > 0){
+			mainContainer.css('left', margins / 2);
+		}
 	}
 }
